4e8ba489b026b4f676e30ade5342246188f5c7b7
tallessouza <talles-swd@live.com>
2022-05-16 20:07:34 -0300

    Arrow

---
+++ arrow.js
@@ -0,0 +1,23 @@
+function soma(x,y) {
+  return x + y
+}
+
+console.log(soma(5,5))
+
+
+const multiplica = function (x, y) {
+  return x * y
+}
+
+console.log(multiplica(5,5))
+
+const dividir = (x,y) => {
+  return x / y
+}
+
+console.log(dividir(5,5))
+
+const obj = () => ({
+  nome: 'Talles',
+  sobre:  'Bonito'
+})
